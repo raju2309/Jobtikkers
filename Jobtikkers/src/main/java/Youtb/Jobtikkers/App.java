3a15727c1be32cec0904c1392f6d53c4b50d5b91
raju2309 <56440664+raju2309@users.noreply.github.com>
2022-03-23 11:18:09 +0530

    First Commit.

---
+++ Jobtikkers/src/main/java/Youtb/Jobtikkers/App.java
@@ -0,0 +1,13 @@
+package Youtb.Jobtikkers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
